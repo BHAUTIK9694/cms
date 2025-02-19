41c15c145b3522e98d3824c00342e359ff4cc28c
BHAUTIK9694 <bhautikkotadiya9694@gmail.com>
2025-02-19 23:42:19 +0530

    client page db changed

--- partials/sql-connction.php
+++ partials/sql-connction.php
@@ -1,14 +1,25 @@
 <?php
-$servername = "localhost"; // or "127.0.0.1"
-$username = "root";        // your MySQL username
-$password = "";            // your MySQL password
-$dbname = "client_manager"; // your database name
-$port = "3306"; // your MySQL server port
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    if ($_SERVER['HTTP_HOST'] === 'cms.celaenotechnology.com') {
+        $servername = "localhost";
+        $username = "u603122711_bhautikkotadiy";
+        $password = "@Hii.2412";
+        $dbname = "u603122711_cms";
+
+    } else {
+    
+        $servername = "localhost"; 
+        $username = "root";      
+        $password = "";          
+        $dbname = "client_manager"; 
+
+    }
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
